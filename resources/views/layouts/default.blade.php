af4b745b73bacc8847af479b9480d21caaf00f35
Jacklu <jacklu.net@gmail.com>
2019-01-16 13:01:04 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title','Sample')</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    @include('layouts._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    @yield('content')
+    @include('layouts._footer')
+</div>
+</body>
 </html>
